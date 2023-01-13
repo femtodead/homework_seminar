4021a6efedc02d1787a678ee7bffe5038aa2aa93
Александр <femt0dead3@mail.ru>
2023-01-13 22:52:43 +0300

    dz_1

---
+++ homework001/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число a: ");
+int first_number = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int second_number = int.Parse(Console.ReadLine());
+int max = first_number;
+
+if (first_number < second_number)
+{
+    max = second_number ;
+}
+Console.Write("a=");
+Console.Write($"{first_number};");
+Console.Write(" b=");
+Console.Write($"{second_number};");
+Console.Write(" -> max=");
+Console.Write($"{max};");
