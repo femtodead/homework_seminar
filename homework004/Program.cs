4288b94f9356f0510f8d31c9a7ef52d25e479ce3
Александр <femt0dead3@mail.ru>
2023-01-14 00:22:13 +0300

    dz_4

---
+++ homework004/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write($"{number}->");
+
+for (int count = 0; count<number-1; count++) 
+{
+ if (count%2 == 0) 
+ {
+   Console.Write($"{count}");
+ }
+ else
+ {
+   Console.Write(",");
+ }
+}
