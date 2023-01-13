1358f1c1c87327ad242f951f7f18f33d66e1395a
Александр <femt0dead3@mail.ru>
2023-01-13 23:30:28 +0300

    dz_3

---
+++ homework003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number%2 == 0)
+{
+    Console.WriteLine($"{number}->да");
+}
+else
+{
+    Console.WriteLine($"{number}->нет");
+}
