cea37d00c00c52ea1834895a418a9c090c7566bd
Александр <femt0dead3@mail.ru>
2023-02-02 23:44:32 +0300

    dz_41_43

---
+++ homework019/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+string UserInput(string text)
+{
+ Console.Write($"{text}: ");
+ string numbers = Console.ReadLine();
+ return numbers;
+}
+
+int[] ArrayNumbers(string BunchOfNumbers)
+{
+char[] separators = new char[] {',', ' '};
+string[] array = BunchOfNumbers.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+int[] newArray = new int[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    newArray[i] = int.Parse(array[i]);
+}
+return newArray;
+}
+
+void NumberPositiveArray(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0) count++; 
+    }
+    Console.Write($" чисел больше нуля в масеве {count} штук");
+}
+
+void main()
+{
+Console.Clear();
+string num = UserInput("Введите цифры через запятую и пробел");
+Console.Write($"[{String.Join(",", ArrayNumbers(num))}]");
+NumberPositiveArray(ArrayNumbers(num));
+}
+
+main();
---
+++ homework020/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int UserInput(string text)
+{
+ Console.Write($"{text}: ");
+ int numbers = int.Parse(Console.ReadLine());
+ return numbers;
+}
+
+void IntersectionPoint(double k1,double b1,double k2,double b2)
+{
+    if (k1==k2)
+    {
+        if (b1==b2) Console.Write("Прямые совпадают");
+        else Console.Write("Прямые паралельны");
+    }
+    else
+    {
+    double x = (b2-b1)/(k1-k2) ; 
+    double y = k2*x+b2    ;
+    Console.Write($"Точка пересечения равна : ({x};{y}) ");
+    }
+}
+
+void main()
+{
+    int k1 = UserInput("Введите переменную k1");
+    int b1 = UserInput("Введите переменную b1");
+    int k2 = UserInput("Введите переменную k2");
+    int b2 = UserInput("Введите переменную b2");
+    IntersectionPoint(k1,b1,k2,b2);
+}
+main();
