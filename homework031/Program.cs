f9bc3498502a83f672f2e3e61c272c4e3ef47ea2
Александр <femt0dead3@mail.ru>
2023-02-13 17:49:38 +0300

    dz_64_66_64

---
+++ homework031/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumber(int m, int n)
+{
+    if (m == n) return n;
+    else
+    {
+        return  n+SumNumber( m, n-1);
+    }
+}
+
+Console.WriteLine(SumNumber(1, 15));
---
+++ homework032/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int Ackermann (int m, int n )
+{
+    if (m == 0) return n+1;
+    else
+    {
+        if (m !=0 && n == 0) return Ackermann (m-1,1);
+        else 
+        {
+            return Ackermann (m-1, Ackermann(m, n-1));
+        }
+    }
+}
+
+Console.WriteLine(Ackermann(2, 3));
